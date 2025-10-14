cc03f2af368d2d82e86524e8634302e8b6667996
rasidi3112 <macbook@Rasidi-MacBook-Pro.local>
2025-10-14 15:54:04 +0700

    math-kids-genius

--- server/index.ts
+++ server/index.ts
@@ -60,12 +60,9 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+  const PORT = 3000; // ganti dari 5000 ke 3000 atau port lain yang bebas
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 })();
